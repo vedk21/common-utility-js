98c1e7c76f7bd76dd1b6af9377f0a8cc56df84e6
ved_k <vedant.kakade@iauro.com>
2017-09-30 03:51:51 +0530

    added json return data functions, published 0.2.7

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SampleComponent } from './sample.component';
 import { SampleDirective } from './sample.directive';
